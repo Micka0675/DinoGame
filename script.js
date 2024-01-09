5a906f7d026d45264080784510ef7a9a92beac73
Micka0675 <94828882+Micka0675@users.noreply.github.com>
2024-01-09 11:42:42 +0100

    EXT: Ajout d'une hitbox et d'une barre de vie

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ let startButton = document.getElementById("startGame");
 let player = document.getElementsByClassName("player")[0];
 let soundTrack = document.getElementById("soundTrack");
 let theBoss = document.getElementsByClassName('stone-giant')[0];
+let lifeBar = document.getElementsByClassName('life-bar')[0];
+let lifeBarContent = document.getElementsByClassName('life-bar-content')[0];
 let toggle = true;
 var walk;
+let health = 0;
+let lives = 3;
+let dammage = 100 / lives; 
+
 //commencer une partie
 startButton.addEventListener('click',play);
 //mise en place du jeu
 function play(){
     //---------------------PAR DEFAUT--------------------//
+    //santé du joueur
+    health = 100;
     //recupération de l'élément du dom espace de jeu
     let gameSpace = document.getElementById("gameSpace");
     //on enlève le button play
@@ -34,11 +42,14 @@ function play(){
     //aparition du boss
     theBoss.style.display='block';
     theBoss.style.delay = '5s';
-    
+    lifeBar.style.visibility = 'visible';
+    lifeBarContent.style.width = '0%';
+    //remplissage de la barre de vie
+    lifeBarContent.style.width = '100%';
     setTimeout(()=>{
         theBoss.style.left = '92%';
         generateWalls();
-    },5000); 
+    },5000);
     theBoss.style.animation = 'bossMove 10s infinite';
     //ajout des écoutes d'évenement sur l'espace de jeu
     document.addEventListener('keydown', function(Event){toggle = movement(Event,toggle)});
@@ -161,20 +172,90 @@ async function generateWalls(){
 };
 
 async function moveTheWall(bloc){
-    // var interval = new Promise(setTimeout(() => {
-    //     bloc.style.animationDelay = '3s';
-    //     bloc.style.animation = 'wallMove 4s running';
-    // }, interval));
-
     var interval = new Promise((resolve,reject) => {
         let delay = Math.floor(Math.random()*3000)+6000;
-
+        let touchAnim = null;
+        bloc.style.animation = 'wallMove 4s running';
+        let timer = setInterval(()=>{
+            //on vérifie s'il y a collision
+            if(isCollide(bloc,player)){
+                //s'il y a collision
+                if(touchAnim === null)
+                {
+                    touchAnim = blink(player,1000);
+                    health = health - dammage;
+                    if(health > 0)
+                    {
+                        lifeBarContent.style.width = health+'%';
+                    }
+                    else
+                    {
+                        gameOver();
+                    }
+                    console.log('vie restante:'+ health);
+                }
+            }
+        },100);
         setTimeout(()=>{
-            bloc.style.animation = 'wallMove 4s running';
-
+            clearInterval(timer);
             //promesse de renvoyer bloc une fois les instructions précédentes réalisées
             resolve(bloc);
         },delay);
     });
     return interval;
 }
+//fonction de vérification de collision
+function isCollide(a,b){
+    let aRect = a.getBoundingClientRect();
+    let bRect = b.getBoundingClientRect();
+    return!(
+        //si le bloc passe au dessus
+        ((aRect.top + aRect.height) < (bRect.top)) ||
+        //si le bloc passe en dessous
+        (aRect.top > (bRect.top + bRect.height)) ||
+        //si le bloc est passé derrière le joueur
+        ((aRect.left + aRect.width) < bRect.left) ||
+        //si le bloc est avant le joueur
+        (aRect.left > (bRect.left + bRect.width)));
+}
+
+//fonction de clignotement en cas de collision
+function blink(elem,duration)
+{
+    //heure du début
+    let timeStart = Date.now();
+    //booléen de visibilité
+    let visible = true;
+    //intervalle de clignotement
+    let intVal = setInterval(()=>{
+        //calcul du temps écoulé
+        let timePassed = Date.now()-timeStart; 
+        //si le temps écoulé est plus élevé ou égal à la durée paramétrée
+        if(timePassed >= duration)
+        {
+            clearInterval(intVal);
+            elem.style.visibility = 'visible';
+            return;
+        }
+        else
+        {
+            // on change la valeur de visibilité de notre élément en fonction de la valeur de visible
+            elem.style.visibility = visible ? 'hidden' : 'visible';
+            //on inverse la valeur de visible en conséquence
+            visible = !visible;
+        }
+    },50);
+    return intVal;
+}
+
+function gameOver(){
+    gameSpace.style.display = 'none';
+    soundTrack.removeEventListener('ended',function(){
+        this.currenTime = 0;
+        this.play();
+    });
+    soundTrack.pause();
+    soundTrack.currenTime = 0;
+    document.body.innerHTML = "You lose";
+
+};
